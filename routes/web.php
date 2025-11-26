ac500d6294f1e93cd6ec3ec2cdcc6b74b5a38aab
3B232073 <3b232073@gm.student.ncut.edu.tw>
2025-11-26 10:40:03 +0800

    用save方式新增資料

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,20 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\Post;
 
-Route::get('/', function () {
-    return view('welcome');
+
+Route::get('//save-post', function () {
+    // 1. 先產生 Post 的物件
+    $post = new Post();
+
+    // 2. 指定欄位內容
+    $post->title = 'test title';
+    $post->content = 'test content';
+    $post->is_feature = false;
+
+    // 3. 儲存到資料庫
+    $post->save();
+
+    return "save() 已新增一筆資料！";
 });
