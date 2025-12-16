80ac042d68908e19133ffab85296667e496d79ba
3B232073 <3b232073@gm.student.ncut.edu.tw>
2025-12-16 23:07:25 +0800

    完成 Post Seeder 與 Factory 假資料設定

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,9 +4,12 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'title',
         'content',
